06c9ff8ff9ac5a79258615e837d19cf76d6f7bdb
talktokris <talktokris@gmail.com>
2022-07-14 23:21:31 +0545

    first commit

---
+++ app/Models/experience_level_list.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class experience_level_list extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/job_ads_job_specification.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class job_ads_job_specification extends Model
+{
+    use HasFactory;
+}
